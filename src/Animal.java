df3a69f056a8a7a507a5c436f4e6178abe25405b
justin-mccartney <justin.mccartney@csuglobal.edu>
2024-04-12 18:05:39 -0500

    Week 1 Commit - Animal Class First Take

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    void eat() {
+        System.out.println("Animal is eating.");
+    }
+}
+
+class dog extends Animal {
+    void bark() {
+        System.out.println("The dog is barking.");
+    }
+}
+
+// In Main ...
+/*
+    Dog dog = new Dog();
+    dog.eat(); ... OUTPUT: "Animal is eating."
+    dog.bark(); ... OUTPUT: "The dog is barking."
+*/
+
+
